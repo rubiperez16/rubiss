0e11b88a4b6c5959ab85e88ab7b96770f7fdb33a
rubiperez16 <114588233+rubiperez16@users.noreply.github.com>
2022-10-21 09:16:06 -0500

    21 de octubre

---
+++ Actividad 2 reloj.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-18">
+        <meta http-equiv="X-Aa-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+</head>
+<body background="fondo.jpg">
+
+<?php
+echo '<center> <font face"ink free" size="7" color="black">';
+echo '<h1>'
+echo '<p>'
+echo date ('d-m-y');
+echo '</h1>'
+echo '</center>'
+?>
+<center>
+    <img src="hola.jpg">
+</center>
+</body>
+</html>
